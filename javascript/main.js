5c0a9639a98a4537bda01eef5af200bbffe09957
shojol shake <dip_shojol@yahoo.com>
2018-12-17 01:38:23 -0500

    adding node.js and some js implementation

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,51 @@
 // alert("this is my js")
-$(window).scroll(function(){
+$(document).ready(function() {
 
-	var wScroll = $(this).scrollTop();
+    $(".welcometext1").css({
+        'animation': 'myWelcomeTextFirst 3s 1'
+    });
+    $(".welcometext1").animate({
+        color: '#000',
+        top: '0px',
+        opacity: 1
+    }, 3000, function() {
+        $(window).scroll(function() {
+            var wScroll = $(this).scrollTop();
 
-	$('.hi').css({
-		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
-	});
+            if (wScroll > 100) {
+                console.log($('.welcometext1:animated').length);
+                    $(".welcometext1").remove()
+                    $(".welcometext2").remove()
+            }
+            $('.hi').css({
+                'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+            });
+        });
+        // Animation complete.
+    });
+    var classwelcome = document.getElementsByClassName('welcometext1');
+
+    console.log(classwelcome + "this");
+
+    // classwelcome.on('click', '.selector', function(event) {
+    // event.preventDefault();
+    /* Act on the event */
 });
+("webkitAnimationEnd", end, false);
+
+function end() {
+    classwelcome.remove();
+}
+
+
+// $('.welcometext1').on("webkitAnimationEnd", myEndFunction);
+
+var myEndFunction = function() {
+    console.log("done");
+}
+
+
+// $('.welcometext1').css({
+// 	'animation: myWelcomeTextFirst 3s 1'
+// });
+// });
