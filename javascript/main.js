f3d79bb053397da3287e73839bba782dd923ad1f
shojol shake <dip_shojol@yahoo.com>
2018-12-27 00:20:41 -0500

    nav hamburger/logo added

--- javascript/main.js
+++ javascript/main.js
@@ -1,56 +1,42 @@
 // alert("this is my js")
 $(document).ready(function() {
-
-	// appding paralax into html
-	var paralax = ""
-
-	// $(".parallax").append('<section class="firstsection"><h1 class="hi">Hi</h1></section>');
-
-    $(".welcometext1").css({
-        'animation': 'myWelcomeTextFirst 3s 1'
+    $('nav').click(function(){
+        $('.nav-wrapper').toggleClass('navClicked');
     });
     $(".welcometext1").animate({
         color: '#000',
         top: '0px',
         opacity: 1
-    }, 3000, function() {
-        $(window).scroll(function() {
-            var wScroll = $(this).scrollTop();
+    }, 4000, function() {
+        $(".welcometext1").animate({
+            color: '#fff',
+            top: '25px',
+            opacity: 0
+        }, 1000)
+    });
 
-            if (wScroll > 100) {
-                console.log($('.welcometext1:animated').length);
-                    $(".welcometext1").remove()
-                    $(".welcometext2").remove()
-            }
-            $('.hi').css({
-                'transform': 'translate(0px, ' + wScroll / 2 + '%)'
-            });
-        });
+    $(".welcometext2").animate({
+        color: '#000',
+        top: '50',
+        opacity: 1
+    }, 4000, function() {
+        $(".welcometext2").animate({
+            color: '#fff',
+            top: '25',
+            opacity: 0
+        }, 2000)
         // Animation complete.
     });
-    var classwelcome = document.getElementsByClassName('welcometext1');
 
-    console.log(classwelcome + "this");
+    $(window).scroll(function() {
+        var wScroll = $(this).scrollTop();
+        $('.hi').css({
+            'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+        });
+    });
 
     // classwelcome.on('click', '.selector', function(event) {
     // event.preventDefault();
     /* Act on the event */
+
 });
-("webkitAnimationEnd", end, false);
-
-function end() {
-    classwelcome.remove();
-}
-
-
-// $('.welcometext1').on("webkitAnimationEnd", myEndFunction);
-
-var myEndFunction = function() {
-    console.log("done");
-}
-
-
-// $('.welcometext1').css({
-// 	'animation: myWelcomeTextFirst 3s 1'
-// });
-// });
